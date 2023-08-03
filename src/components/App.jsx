2edd2ced8e15a9bb620ee5fd541699f855e60045
Kristina <106387313+KristinaBiba@users.noreply.github.com>
2023-08-03 17:02:07 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,3 +14,36 @@ export const App = () => {
     </div>
   );
 };
+// import { lazy, useEffect } from "react";
+// import { Route, Routes } from "react-router-dom";
+// import { SharedLayout } from "components/SharedLayout/SharedLayout";
+// import { useAuth } from "redux/Auth/useAuth";
+// import { refreshUser } from "redux/Auth/authOperations";
+// import { useDispatch } from "react-redux";
+// import { RestrictedRoute } from "./RestrictedRoute";
+// import { PrivateRoute } from "./PrivateRoute";
+
+// const Login = lazy(() => import("pages/Login"));
+// const Register = lazy(() => import("pages/Register"));
+// const Contacts = lazy(() => import("pages/Contacts"));
+
+// export function App() {
+
+//   const dispatch = useDispatch();
+//   const { isRefreshing } = useAuth();
+
+//   useEffect(() => {
+//     dispatch(refreshUser());
+//   }, [dispatch]);
+
+//      return ( isRefreshing ? <div>Loading...</div> :
+// (       <Routes>
+//         <Route path="/" element={<SharedLayout/>}>
+//             <Route path="login" element={<RestrictedRoute redirectTo="/contacts" component={<Login />} />}></Route>
+//             <Route path="register" element={<RestrictedRoute redirectTo="/contacts" component={<Register />} />}></Route>
+//            <Route path="contacts" element={<PrivateRoute redirectTo="/login" component={<Contacts />} />}></Route>
+//          </Route>  
+//         <Route path="*" element={<>NotFound</>} /> 
+//       </Routes>)
+//     );
+// };
