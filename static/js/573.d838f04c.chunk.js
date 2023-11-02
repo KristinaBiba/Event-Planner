844fdf8d93eea7df98eb60d6d7e20be0ec1ac4ed
KristinaBiba <106387313+KristinaBiba@users.noreply.github.com>
2023-11-02 16:52:53 +0000

    Deploying to gh-pages from @ KristinaBiba/Event-Planner@71d9a4ef87c00ca46c5135258807b017bcac780d 🚀

---
+++ static/js/573.d838f04c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkevent_planner=self.webpackChunkevent_planner||[]).push([[573],{7573:function(e,n,t){t.r(n),t.d(n,{CreatePage:function(){return l}});t(2791);var r=t(9581),a=t(9414),i=t(7642),s=t(2830),u=t(1586),c=t(184);function l(e){var n=e.onSubmit;return(0,c.jsx)("main",{children:(0,c.jsx)(s.$,{children:(0,c.jsxs)(a.W,{children:[(0,c.jsx)(i.F,{to:"/",title:"Back"}),(0,c.jsx)(u.H2,{title:"Create new event"}),(0,c.jsx)(r.$,{onSubmit:n})]})})})}n.default=l}}]);
+//# sourceMappingURL=573.d838f04c.chunk.js.map
